2f0d1e5d900034c61f04e574d5370e4ddf7fd57a
Tom Wilson <tom@iwm.pw>
2018-01-02 21:29:33 +0000

    Empty project

---
+++ Middleware/src/middleware/Middleware.java
@@ -0,0 +1,17 @@
+package middleware;
+
+/**
+ * @author tom
+ */
+public class Middleware
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        // TODO code application logic here
+    }
+
+}
